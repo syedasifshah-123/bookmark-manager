a5e64183924ae8a143f6f48b69fb2a65bbb6db82
Syed Asif Shah <syedasifshah582@gmail.com>
2026-03-28 01:41:45 +0500

    fix api url env variable

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:4000/api',
+    baseURL: "https://bookmark-manager-production-f86e.up.railway.app/api",
 });
 
 
